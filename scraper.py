278511db5021ee87b6c57502d6c0d32650cbc3a4
Axel <44074386+Silterman@users.noreply.github.com>
2023-09-18 20:11:33 +0200

    Update scraper.py
    Added auto-rename functionality. Still restricted to multi season shows.

--- scraper.py
+++ scraper.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup as BS
 import requests
-import csv
+import os
 
 link = "https://thetvdb.com/series/house/allseasons/official"
+#link = "https://thetvdb.com/series/the-mentalist/allseasons/official"
+
+numSeasons = 8
+
 page = requests.get(link)
+
+dest = "B:\\Emby\\Series\\House\\"
 
 soup = BS(page.content, "html.parser") #grabs entire page HTML and turns it into some sort of class
 
@@ -11,8 +17,7 @@ seasons = soup.find_all('ul',attrs={"class" : "list-group"})
 #print(seasons)
 
 def findSeasonEpisodes(season, n):
-    if len(n) == 1:
-        n = "0" + n
+    destLocal = dest + f"Season {n}"
     output = {}
     episodesBundle = [name.get_text().strip() for name in season.find_all("h4", attrs={"class": "list-group-item-heading"}) if name.get_text().strip() != ""]
     episodes = [episode for episode in episodesBundle if 'SPECIAL' not in episode]
@@ -20,8 +25,9 @@ def findSeasonEpisodes(season, n):
         tempEpisode = episodes[i].replace("\n", "").split("                                    ")
         episodes[i] = tempEpisode
         output.update({episodes[i][0][episodes[i][0].index("E"):]:episodes[i][1]})
-    print(n ,output)
+    #print(output)
+    print(os.listdir(destLocal))
     return output
 
-for i in range(len(seasons))[:-1]:
+for i in range(len(seasons[:numSeasons])):
     findSeasonEpisodes(seasons[i], str(i+1))
